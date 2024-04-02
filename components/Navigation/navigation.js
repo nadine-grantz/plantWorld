2a0acd195999b4af7e21ef8882e5600d99e0cf3f
Nadine Grantz <nadine.grantz@googlemail.com>
2024-04-02 12:25:46 +0200

    create styling for navigation

--- components/Navigation/navigation.js
+++ components/Navigation/navigation.js
@@ -1,4 +1,104 @@
 import { useState } from "react";
+import styled from "styled-components";
+
+import Link from "next/link";
+
+export const NavigationContainer = styled.nav`
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+export const NavIcon = styled.div`
+  position: absolute;
+  top: 1.125rem;
+  left: 1rem;
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  width: 36px;
+  aspect-ratio: 1;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: var(--border-radius_square-button);
+  padding: 9px 7px;
+  cursor: pointer;
+  z-index: 3;
+
+  span {
+    height: 3px;
+    background-color: var(--color_white);
+    border-radius: 3px;
+    transition: var(--transition_button);
+
+    &:nth-child(1) {
+      transform: ${({ $isOpen }) =>
+        $isOpen ? "rotate(45deg) translate(5px, 5px)" : "rotate(0)"};
+    }
+
+    &:nth-child(2) {
+      opacity: ${({ $isOpen }) => ($isOpen ? 0 : 1)};
+    }
+
+    &:nth-child(3) {
+      transform: ${({ $isOpen }) =>
+        $isOpen ? "rotate(-45deg) translate(5px, -5px)" : "rotate(0)"};
+    }
+  }
+`;
+
+export const NavMenu = styled.div`
+  position: fixed;
+  top: 0;
+  left: ${({ $isOpen }) => ($isOpen ? "0" : "calc(-100% - 1rem)")};
+  width: 100vw;
+  height: 100vh;
+  background-color: var(--color_midnight);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: left 0.3s;
+  z-index: 2;
+  overflow: hidden;
+
+  ul {
+    list-style: none;
+    width: 100%;
+    overflow: hidden;
+    position: relative;
+    z-index: 2;
+  }
+`;
+
+export const NavLink = styled(Link)`
+  display: block;
+  width: 100%;
+  padding: 1rem;
+  font: var(--font_heading-3);
+  color: var(--color_white);
+  text-decoration: none;
+  text-transform: uppercase;
+  text-align: center;
+
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.04);
+  }
+`;
+export const NavButton = styled.button`
+  display: block;
+  width: 100%;
+  padding: 1rem;
+  font: var(--font_heading-3);
+  color: var(--color_white);
+  background-color: var(--color_midnight);
+  border: none;
+  cursor: pointer;
+  text-transform: uppercase;
+  text-align: center;
+
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.04);
+  }
+`;
 
 export default function Navigation() {
   const [isNavigationVisible, setVisibleNavigation] = useState(false);
@@ -13,30 +113,30 @@ export default function Navigation() {
 
   return (
     <>
-      <nav>
-        <button onClick={toggleNavigation}>Burger Menu hier</button>
-        {isNavigationVisible && (
+      <NavigationContainer>
+        <NavButton onClick={() => toggleNavigation()}>Navigation</NavButton>
+        <NavMenu $isOpen={isNavigationVisible}>
           <ul>
             <li>
-              <a href="/" onClick={handleLinkClick}>
+              <NavLink href="/" onClick={handleLinkClick}>
                 Home
-              </a>
+              </NavLink>
             </li>
 
             <li>
-              <a href="/add-own-plant" onClick={handleLinkClick}>
+              <NavLink href="/add-own-plant" onClick={handleLinkClick}>
                 AddOwnPlants
-              </a>
+              </NavLink>
             </li>
 
             <li>
-              <a href="/my-plants" onClick={handleLinkClick}>
+              <NavLink href="/my-plants" onClick={handleLinkClick}>
                 My Plants
-              </a>
+              </NavLink>
             </li>
           </ul>
-        )}
-      </nav>
+        </NavMenu>
+      </NavigationContainer>
     </>
   );
 }
--- pages/add-own-plant.js
+++ pages/add-own-plant.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 // hier sollen alle pflanzen dargestellt werden und mit hilfe von einer bookmark hinzugefügt werden können
 // zu my-plants. Es muss eine Suchfunktion geben in der man nach z.B. care, watering, sun etc filtern kann
 
-export default function AddOwnPlants() {
+export default function AddOwnPlants({ plants }) {
   return (
     <>
       <h1>Add my Plants</h1>
