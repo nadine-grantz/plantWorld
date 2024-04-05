888ee37c54666473ec1b92bd5a83873f7032b54d
Nadine Grantz <nadine.grantz@googlemail.com>
2024-04-05 21:26:21 +0200

    create homepage

--- components/Forms/PlantList.js
+++ components/Forms/PlantList.js
@@ -1,4 +1,5 @@
 import plants from "../../lib/plants.json";
+import Link from "next/link";
 
 export default function PlantList({ plants }) {
   return (
@@ -7,11 +8,13 @@ export default function PlantList({ plants }) {
         {plants.map((plant) => (
           <li key={plant.id}>
             <h2>{plant.title}</h2>
-            <p>{plant.description}</p>
+            {/* <p>{plant.description}</p>
             <p>{plant.location}</p>
             <p>{plant.watering}</p>
-            <p>{plant.care}</p>
-            <img src={plant.picture} width={100} height={149} />
+            <p>{plant.care}</p> */}
+            <Link href="/my-plants">
+              <img src={plant.picture} width={100} height={149} />
+            </Link>
           </li>
         ))}
       </ul>
