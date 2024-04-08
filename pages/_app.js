a564d59ce9d1273e74f4b6f4064ee6115e5bee97
Nadine Grantz <nadine.grantz@googlemail.com>
2024-04-08 22:06:18 +0200

    create first togglebutton for favorite plants

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,40 @@
 import GlobalStyle from "../styles";
 import Navigation from "@/components/Navigation/navigation";
 import plants from "../lib/plants.json";
+import useLocalStorageState from "use-local-storage-state";
 
 export default function App({ Component, pageProps }) {
+  const [plantsInfo, setPlantsInfo] = useLocalStorageState("plantsInfo", []);
+
+  // Überprüfe, ob plantsInfo undefined ist und setze es auf ein leeres Array, falls erforderlich
+  if (typeof plantsInfo === "undefined") {
+    setPlantsInfo([]);
+  }
+
+  function handleToggleBookmark(id) {
+    const currentPlant = plantsInfo.find((plant) => plant.id === id);
+    if (currentPlant) {
+      setPlantsInfo(
+        plantsInfo.map((plantInfo) =>
+          plantInfo.id === id
+            ? { ...plantInfo, isBookmarked: !plantInfo.isBookmarked }
+            : plantInfo
+        )
+      );
+    } else {
+      setPlantsInfo([...plantsInfo, { id, isBookmarked: true }]);
+    }
+  }
+
   return (
     <>
       <GlobalStyle />
       <Navigation />
-      <Component {...pageProps} plants={plants} />
+      <Component
+        {...pageProps}
+        plants={plants}
+        handleToggleBookmark={handleToggleBookmark}
+      />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function Homepage({ plants }) {
+export default function Homepage({ plants, handleToggleBookmark }) {
   return (
     <>
       <h2>Welcome to your PlantWorld // index.js</h2>
@@ -8,6 +8,9 @@ export default function Homepage({ plants }) {
         {plants.map((plant) => (
           <li key={plant.id}>
             <h2>{plant.title}</h2>
+            <button onClick={() => handleToggleBookmark(plant.id)}>
+              hier ist ein button
+            </button>
             <Link href={`/plant-details/${plant.id}`} key={plant.id}>
               <img src={plant.picture} width={100} height={149} />
             </Link>
