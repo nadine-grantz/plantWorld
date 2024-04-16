7da83cf9ba1acd7c82d3298d997470b7cdc5c3e0
Nadine Grantz <nadine.grantz@googlemail.com>
2024-04-16 18:03:05 +0200

    update setFavoritePlants

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,7 @@ export default function App({ Component, pageProps }) {
     const updatedFavorites = favoritePlants.filter(
       (plant) => plant.id !== plantToRemove.id
     );
+    setFavoritePlants(updatedFavorites);
   }
   return (
     <>
